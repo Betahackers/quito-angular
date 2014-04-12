624e6cc001b4279a8493365855d761f1aa628a73
Andrej Dragisic <andrejdragisic@gmail.com>
2014-04-12 16:46:39 +0200

    Bootstrapped app with yeoman and bower installs

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('quitoApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
